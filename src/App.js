01889b77f6915c38d3167321c4a47a85d845ee59
thedeepakmaurya <mauryadeepak135@gmail.com>
2024-07-04 02:37:37 +0530

    mobile view fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { FirebaseProvider } from "./utils/Firebase";
+import notify from "./assets/img/notify.svg";
 
 function App() {
   return (
     <FirebaseProvider>
-      <Navbar />
-      <Outlet />
+      <div className="sm:visible sm:flex sm:flex-col gap-5 hidden items-center h-screen justify-center">
+        <img src={notify} alt="notify" className="w-44" />
+        <h1 className="font-bold text-2xl text-primaryBlue ">Accessible on desktop!</h1>
+      </div>
+      <div className="sm:hidden md:hidden">
+        <Navbar />
+        <Outlet />
+      </div>
     </FirebaseProvider>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'sm': { 'min': '320px', 'max': '639px' },
+
+      'md': { 'min': '640px', 'max': '1023px' },
+
+      'lg': { 'min': '1024px', 'max': '1279px' },
+
+      'xl': { 'min': '1280px', 'max': '1535px' },
+
+      '2xl': { 'min': '1536px' },
+    },
     extend: {
-      colors:{
+      colors: {
         primaryBlue: '#0FA3B1',
         secondaryBlue: '#B5E2FA',
         offWhite: '#F9F7F3',
